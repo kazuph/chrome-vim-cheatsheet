9234629c635d88968bf01658ad58a012ef084523
Kazuhiro Homma <kazu.homma@gmail.com>
2013-02-13 01:06:48 +0900

    first commit

---
+++ newTab.js
@@ -0,0 +1,7 @@
+var urls = [
+'https://raw.github.com/LevelbossMike/vim_shortcut_wallpaper/master/vim-shortcuts_1280x800.png',
+'https://raw.github.com/LevelbossMike/vim_shortcut_wallpaper/master/vim-shortcuts-dark_1280x800.png',
+'https://raw.github.com/LevelbossMike/vim_shortcut_wallpaper/master/vim-shortcuts-background_1280x1080.png',
+];
+
+location.href = urls[ Math.floor( Math.random() * urls.length ) ]
